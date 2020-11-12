8ccf4eace69033ec512ac4450e4f936cbe5a054c
Michael Chen <michaelchengaming@gmail.com>
2020-11-11 23:55:53 -0800

    add script to obstacles and make em move up and down

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    //adjust this to change speed
+    [SerializeField] float speed = 5f;
+    //adjust this to change how high it goes
+    [SerializeField] float height = 0.5f;
+
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
